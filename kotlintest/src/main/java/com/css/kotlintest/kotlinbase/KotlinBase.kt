9a6df295ee5c176761d7f607137383024d1ca803
css <censhaoshuai2008@126.com>
2018-10-18 11:03:47 +0800

    add kotlin learn

--- kotlintest/src/main/java/com/css/kotlintest/kotlinbase/KotlinBase.kt
+++ kotlintest/src/main/java/com/css/kotlintest/kotlinbase/KotlinBase.kt
@@ -1,7 +1,20 @@
 package com.css.kotlintest.kotlinbase
 
-class KotlinBase{
-    fun main(){
+class KotlinBase {
+    fun main() {
         println("hello world")
+    }
+
+    fun sum(a: Int, b: Int): Int {
+        println("sum of $a and $b is ${a + b}")
+        return a + b
+    }
+
+    fun isranges(){
+        val x = 10
+        val y = 9
+        if (x in 0..y){
+            println("fits in range")
+        }
     }
 }
--- kotlintest/src/test/java/com/css/kotlintest/ExampleUnitTest.kt
+++ kotlintest/src/test/java/com/css/kotlintest/ExampleUnitTest.kt
@@ -4,6 +4,7 @@ import com.css.kotlintest.kotlinbase.KotlinBase
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,9 +16,37 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    fun mul(a: Int, b: Int) = a * b
     @Test
-    fun mytest(){
+    fun mytest() {
         val base = KotlinBase()
         base.main()
+        base.isranges()
+    }
+
+    @Test
+    fun mytest2() {
+        for (x in 1..10 step 2) {
+            print(x)
+        }
+        println()
+        for (x in 9 downTo 0 step 3) {
+            print(x)
+        }
+    }
+
+    @Test
+    fun mytestCollection() {
+        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+        fruits
+                .filter { it.startsWith("a") }
+                .sortedBy { it }
+                .map { it.toUpperCase() }
+                .forEach { println(it) }
+    }
+
+    @Test
+    fun filetest(){
     }
 }
