fa6733b50ba08dcdeb37113af541d8763ab4c1c9
css <censhaoshuai2008@126.com>
2019-02-14 16:37:17 +0800

    递减函数

--- applab/src/test/java/css/com/applab/ExampleUnitTest.java
+++ applab/src/test/java/css/com/applab/ExampleUnitTest.java
@@ -4,8 +4,6 @@ import android.content.pm.PackageManager;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,10 +11,19 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     PackageManager packageManager;
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        System.out.println(input(0));
 
-//        packageManager.buildRequestPermissionsIntent
+    }
+
+    /**
+     * @param value 400-1
+     * @return 0-1
+     */
+    public float input(int value) {
+        float e = 1f / 400;
+        return 1 - value * e;
     }
 }
