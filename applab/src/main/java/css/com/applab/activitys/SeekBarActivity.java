01109cff956eac2eb79237463750abfc5560538d
css <censhaoshuai2008@126.com>
2019-03-16 13:57:18 +0800

    test third part seek bar ui lib

---
+++ applab/src/main/java/css/com/applab/activitys/SeekBarActivity.java
@@ -0,0 +1,15 @@
+package css.com.applab.activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import css.com.applab.R;
+
+public class SeekBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_bar);
+    }
+}
