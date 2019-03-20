3bbdaeb971d478096d5972629ea7ea0e3ed1b5a4
css <censhaoshuai2008@126.com>
2019-03-20 17:54:28 +0800

    context menu test,放弃了，不好用

--- applab/src/main/java/css/com/applab/activitys/ContextMenuActivity.java
+++ applab/src/main/java/css/com/applab/activitys/ContextMenuActivity.java
@@ -1,6 +1,8 @@
 package css.com.applab.activitys;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import css.com.applab.R;
@@ -11,5 +13,11 @@ public class ContextMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_context_menu);
+    }
+
+    public void show(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.context_menu_test);
+        popupMenu.show();
     }
 }
--- kotlintest/src/test/java/com/css/kotlintest/KotlinTest.kt
+++ kotlintest/src/test/java/com/css/kotlintest/KotlinTest.kt
@@ -8,13 +8,7 @@ import org.junit.Test
 class KotlinTest {
     @Test
     fun show() {
-        val wait = arrayListOf<Int>(1, 2, 3, 4, 5)
 
-        //continute braak goto
-        for (n in wait.indices) {
-            println(wait[n])
-            if (n == 2) return
-        }
     }
 
 }
