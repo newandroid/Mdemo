19711b5ec21f0a6043c94ae1831d34805e5ec0a1
css <censhaoshuai2008@126.com>
2019-03-20 18:14:28 +0800

    custom chart view test

---
+++ applab/src/main/java/css/com/applab/activitys/ChartViewActivity.java
@@ -0,0 +1,15 @@
+package css.com.applab.activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import css.com.applab.R;
+
+public class ChartViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_view);
+    }
+}
---
+++ applab/src/main/java/css/com/applab/view/ChartView.java
@@ -0,0 +1,30 @@
+package css.com.applab.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * create by css on 2019/3/20
+ */
+public class ChartView extends View {
+    public ChartView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+    private void init(){
+
+    }
+}
