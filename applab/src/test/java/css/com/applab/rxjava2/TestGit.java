494daff37f81eba79f83c9889a78790ca9959c88
css <censhaoshuai2008@126.com>
2019-07-05 16:13:17 +0800

    git content reset

--- applab/src/test/java/css/com/applab/rxjava2/TestGit.java
+++ applab/src/test/java/css/com/applab/rxjava2/TestGit.java
@@ -4,14 +4,5 @@ package css.com.applab.rxjava2;
  * create by css on 2019/7/5
  */
 public class TestGit {
-    public void add() {
 
-    }
-
-    public void more() {
-
-    }
-    public void moreTwo(){
-        
-    }
 }
