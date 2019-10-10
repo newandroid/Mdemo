acec9ec774786a25bc2724d448d8b35baf41a8c3
css <censhaoshuai2008@126.com>
2019-10-10 21:51:30 +0800

    rxjava pull compare push

--- mrxjava/src/main/java/com/example/mrxjava/MyClass.java
+++ mrxjava/src/main/java/com/example/mrxjava/MyClass.java
@@ -7,18 +7,45 @@ import com.example.mrxjava.reactive.IObservable;
 import com.example.mrxjava.reactive.IObserver;
 
 public class MyClass {
+    static final String CONTENT = "content";
 
     public static void main(String[] args) {
+        //normal implement
+        String dataPull = getDataPull();
+        System.out.println(dataPull);
+
+        //push implement
+        IObservable<String> dataPush = getDataPush();
+        dataPush.subscribe(new IObserver<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+
+            @Override
+            public void onNext(String args) {
+                System.out.println(args);
+            }
+        });
+    }
+
+    private void unkonwTest() {
         IObservable iObservable = WatchableExtensions.create(new Func1<IDisposable, IObserver<String>>() {
             @Override
             public IDisposable call(IObserver<String> iObserver) {
                 iObserver.onNext("hello world");
 //                iObserver.onError(new Exception());
                 try {
-                    Thread.sleep(2*1000);
+                    Thread.sleep(2 * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                iObserver.onNext("delay");
                 iObserver.onCompleted();
                 return null;
             }
@@ -39,9 +66,23 @@ public class MyClass {
                 System.out.println(args);
             }
         });
-        onCompleted.unsubscribe();
+        onCompleted.unsubscribe();//无效
 
-        iObservable.subscribe(new String(""));
+//        iObservable.subscribe(new String(""));
 
+    }
+
+    public static String getDataPull() {
+        return CONTENT;
+    }
+
+    public static IObservable<String> getDataPush() {
+        return WatchableExtensions.create(new Func1<IDisposable, IObserver<String>>() {
+            @Override
+            public IDisposable call(IObserver<String> iObserver) {
+                iObserver.onNext(CONTENT);
+                return null;
+            }
+        });
     }
 }
