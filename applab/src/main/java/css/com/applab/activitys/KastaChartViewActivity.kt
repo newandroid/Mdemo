1a4d12ddd5e6a257038f6aa4a53126e394ee3343
css <censhaoshuai2008@126.com>
2019-11-07 21:29:04 +0800

    chart view

---
+++ applab/src/main/java/css/com/applab/activitys/KastaChartViewActivity.kt
@@ -0,0 +1,13 @@
+package css.com.applab.activitys
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import css.com.applab.R
+
+class KastaChartViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kasta_chart_view)
+    }
+}
